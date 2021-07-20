d7e3f097b8b6b3e51820f410a5e4db6f7edd92bd
sirsavant <stanleyho619@gmail.com>
2021-07-20 15:52:44 -0400

    added additional functionality

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,7 +17,10 @@ DONE - a count down of rock, paper, scissors after the player makes a choice.
 - a reset button.
 - bot 
 --
-
+count the number of rounds
+--
+make the game go again.
+--
 rock > scissors
 scissors > paper
 paper > rock
@@ -45,87 +48,125 @@ const bot = {
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-let delayInMilliseconds = 1000;
+let delayInMilliseconds = 100;
+let round = 0;
+let ties = 0;
+let result = undefined; 
+let playAgain = undefined;
 
-
-console.log("*****SCOREBOARD******");
-console.log("**Player Wins: " + player.playerWins);
-console.log("**Player Losses: " + player.playerLosses);
-console.log("*********************");
-console.log("**Bot Wins: " + bot.botWins);
-console.log("**Bot Losses: " + bot.botLosses);
-console.log("*********************");
-
-// A function to assign a random number for the bot to use in choosing either rock, paper, or scissors.
-function random1To3() {
-  return Math.round(Math.random() * 2 + 1);
-}
-botChoice = random1To3();
-
-if (botChoice == 1) {
-  bot.botChoice = rock;
-}
-else if (botChoice == 2) {
-  bot.botChoice = paper;
-}
-else {
-  bot.botChoice = scissors;
-}
-
-console.log("Bot Choice: " + bot.botChoice);
-
-
-// Prompts the player for a choice.
-do {
-  player.playerChoice = window.prompt("Please enter: Rock, Paper, or Scissors..").toLowerCase();
-} while (player.playerChoice !== "rock" && 
-         player.playerChoice !== "paper" &&
-         player.playerChoice !== "scissors");
-
-console.log("Player choice: " + player.playerChoice);
-
-function gameLogic(playerChoice, botChoice) {
-  let playerWin = "player wins!";
-  let botWin = "bot wins!";
-  if (playerChoice == "rock" && botChoice == "scissors") {
-    console.log(playerWin);
+// A function to assign a random number for the bot to use in choosing either rock, paper, or scissors. 
+function botChoice() {  
+  // A variable called choice that will be assigned a random number 1-3. This will be used for bot.botChoice.
+  let choice = Math.round(Math.random() * 2 + 1);
+  
+  if (choice == 1) {
+    bot.botChoice = rock;
   }
-  else if (playerChoice == "scissors" && botChoice == "paper") {
-    console.log(playerWin);
-  }
-  else if (playerChoice == "paper" && botChoice == "rock") {
-    console.log(playerWin);
-  }
-  else if (botChoice == "rock" && playerChoice == "scissors") {
-    console.log(botWin);
-  }
-  else if (botChoice == "scissors" && playerChoice == "paper") {
-    console.log(botWin);
-  }
-  else if (botChoice == "paper" && playerChoice == "rock") {
-    console.log(botWin);
+  else if (choice == 2) {
+    bot.botChoice = paper;
   }
   else {
-    console.log("Tie!");
+    bot.botChoice = scissors;
+  }
+
+  console.log("Bot choice: " + bot.botChoice);
+}
+
+botChoice();
+
+// Prompts the player for a choice.
+function playerChoice() {
+  do {
+    player.playerChoice = window.prompt("Please enter: Rock, Paper, or Scissors.").toLowerCase();
+  } while (player.playerChoice !== "rock" && player.playerChoice !== "paper" && player.playerChoice !== "scissors");
+
+  console.log("Player choice: " + player.playerChoice);
+}
+
+playerChoice();
+
+function gameLogic(playerChoice, botChoice) {
+  let playerWin = "Player wins!";
+  let botWin = "Bot wins!";
+  let tie = "Tie!";
+
+  if ((playerChoice == "rock" && botChoice == "scissors") || (playerChoice == "scissors" && botChoice == "paper") || (playerChoice == "paper" && botChoice == "rock")) {
+    console.log("************************* " + playerWin);
+    return playerWin;
+  }
+  else if ((botChoice == "rock" && playerChoice == "scissors") || (botChoice == "scissors" && playerChoice == "paper") || (botChoice == "paper" && playerChoice == "rock")) {
+    console.log("************************* " + botWin);
+    return botWin;
+  }
+  else {
+    console.log("************************* " + tie);
+    return tie;
   }
 }
 
-// countdown
-if (player.playerChoice != undefined) {
-  setTimeout(function() {
-    console.log("...");
-    setTimeout(function() {
-      console.log("...Rock!");
-      setTimeout(function() {
-        console.log("...Paper!");
-        setTimeout(function() {
-          console.log("...Scissors!");
-            setTimeout(function() {
-              gameLogic(player.playerChoice, bot.botChoice);
-            }, delayInMilliseconds);
-      }, delayInMilliseconds);
-    }, delayInMilliseconds);
-  }, delayInMilliseconds);
-}, delayInMilliseconds);
+function scoreboard() {
+  console.log("*****SCOREBOARD******");
+  console.log("** Round: " + round);
+  console.log("*********************");
+  console.log("** Player Wins: " + player.playerWins);
+  console.log("** Player Losses: " + player.playerLosses);
+  console.log("*********************");
+  console.log("** Bot Wins: " + bot.botWins);
+  console.log("** Bot Losses: " + bot.botLosses);
+  console.log("*********************");
+  console.log("** Ties: " + ties);
+  console.log("*********************");
 }
 
+function playItAgain(playAgain) {
+    playAgain = window.confirm('Press OK to play again.');
+
+  if (playAgain == true) {
+    botChoice();
+    playerChoice();
+    countdown();
+  }
+}
+
+function countdown() {
+  if (player.playerChoice != undefined) {
+    setTimeout(function() {
+      console.log("...");
+      setTimeout(function() {
+        console.log("...Rock!");
+        setTimeout(function() {
+          console.log("...Paper!");
+          setTimeout(function() {
+            console.log("...Scissors!");
+              setTimeout(function() {
+                result = gameLogic(player.playerChoice, bot.botChoice);
+                
+                if (result == "Player wins!") {
+                  player.playerWins++;
+                  bot.botLosses++;
+                }
+                else if (result == "Bot wins!") {
+                  bot.botWins++;
+                  player.playerLosses++;
+                }
+                else {
+                  ties++;
+                }
+
+                round++;
+
+                scoreboard();
+
+                playAgain = undefined;
+
+                playItAgain(playAgain);
+
+              }, delayInMilliseconds);
+          }, delayInMilliseconds);
+        }, delayInMilliseconds);
+      }, delayInMilliseconds);
+    }, delayInMilliseconds);
+  }
+}
+
+countdown();
